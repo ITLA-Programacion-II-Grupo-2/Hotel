72ae197cbe521f677e510c6ca91fe303b9d444cb
HilariMF <20221025@itla.edu.do>
2023-06-17 02:02:20 -0400

    Inicio de capa API

---
+++ Hotel/Hotel.API/Controllers/RolUsuarioController.cs
@@ -0,0 +1,54 @@
+﻿using Hotel.Domain.Entities;
+using Hotel.Infrastructure.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Hotel.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RolUsuarioController : ControllerBase
+    {
+        private readonly IRolUsuarioRepository iRolUsuarioRepository;
+
+
+        public RolUsuarioController(IRolUsuarioRepository iRolUsuarioRepository)
+        {
+            this.iRolUsuarioRepository = iRolUsuarioRepository;
+
+        }
+
+        [HttpGet("GetRolUsuario")]
+        public IActionResult Get()
+        {
+            var rolusuario = this.iRolUsuarioRepository.GetUsuariosByRoles();
+
+            return Ok(rolusuario);
+        }
+
+      
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+
+        [HttpPost("SaveRolUsuario")]
+        public void Post([FromBody] RolUsuario rolusuario)
+        {
+        }
+
+
+        [HttpPost("UpdateRolUsuario")]
+        public void Put([FromBody] RolUsuario rolusuario)
+        {
+        }
+
+        [HttpPost("Remove ")]
+        public void Delete([FromBody] RolUsuario rolusuario)
+        {
+        }
+    }
+}
---
+++ Hotel/Hotel.API/Controllers/UsuarioController.cs
@@ -0,0 +1,54 @@
+﻿using Hotel.Domain.Entities;
+using Hotel.Infrastructure.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Hotel.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly IUsuarioRepository iUsuarioRepository;
+
+
+        public UsuarioController(IUsuarioRepository iUsuarioRepository)
+        {
+            this.iUsuarioRepository = iUsuarioRepository;
+
+        }
+
+
+        [HttpGet]
+        //public IActionResult Get()
+        //{
+        //    var usuario = this.iUsuarioRepository.GetUsuario(()
+        //    return Ok(usuario); 
+        //}
+
+      
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+
+        [HttpPost("SaveUsuario")]
+        public void Post([FromBody] Usuario usuario)
+        {
+        }
+
+
+        [HttpPost("UpdateUsuario")]
+        public void Put([FromBody] Usuario usuario)
+        {
+        }
+
+        [HttpPost("Remove ")]
+        public void Delete([FromBody] Usuario usuario)
+        {
+        }
+    }
+}
